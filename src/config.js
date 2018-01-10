5ad80d4a206c4a88229ece13d1f19adf9b35128a
reillj <jenniferreilly1992@gmail.com>
2018-01-09 19:08:19 -0700

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  cognito: {
+    USER_POOL_ID: "us-east-2_5UsEG0Q6J",
+    APP_CLIENT_ID: "2c3mmr9s4sgqt95c8faeesv9ns"
+  }
+};
---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Music2YourEars</h1>
+          <p>A Music Forum</p>
+        </div>
+      </div>
+    );
+  }
+}
